e2e5edcd95446532a2d972fd5ffc064860d2f02d
Anastasia Kharitonova <haanastasia@yandex.com>
2021-01-17 01:32:01 +0400

    Задание #3 и #4 из ДЗ 2

--- dz2/index.php
+++ dz2/index.php
@@ -1,4 +1,13 @@
 <?php
 require('src/functions.php');
 
-echo task1(array('test','test2'), true);
+echo task1(array('test', 'test2'), true);
+echo task2('*', 12, 1, 22, 1);
+echo task3(4, 12);
+
+echo '<br><br>'; // разделитель
+
+// Задание #4
+echo date("d.m.Y H:i") . '<br>'; // 17.02.2021 12:59
+$unixTime = strtotime("24.02.2016 00:00:00"); // unixtime время соответствующее 24.02.2016 00:00:00
+echo $unixTime;
--- dz2/src/functions.php
+++ dz2/src/functions.php
@@ -1,4 +1,5 @@
 <?php
+// Задание #1
 function task1(array $strings, $unite = false)
 {
     foreach ($strings as $string) {
@@ -10,12 +11,53 @@ function task1(array $strings, $unite = false)
     }
 }
 
-function task2()
+// Задание #2
+function task2(string $arithmetic, ...$numbers)
 {
-    return 'SOMETHING';
+    $value = 0;
+    if ($arithmetic != '*' and $arithmetic != '/' and $arithmetic != '-' and $arithmetic != '+') {
+        return 'Неправильный арифметический оператор';
+    }
+
+    foreach ($numbers as $number) {
+        if (!is_int($number) and !is_float($number)) {
+            return 'Введите корректное число';
+        }
+        eval("\$value = ($value == 0 ? $number : $value $arithmetic $number);");
+
+    }
+
+    return $value;
 }
 
-function task3()
+// Задание #3
+function task3(int $a, int $b)
 {
-    return 'SOMETHING';
+    if (!is_int($a) or !is_int($b)) {
+        return 'Введите корректное число';
+    }
+
+    if ($a > $b) {
+        return 'Первое число не должно быть больше второго';
+    }
+
+    $rows = $a;
+    $cols = $b;
+    $multiTable = "<table border='1' align='center''>";
+
+    for ($tr = 1; $tr <= $rows; $tr++) {
+
+        $multiTable .= "<tr>";
+
+        for ($td = 1; $td <= $cols; $td++) {
+            $multiTable .= "<td>" . $tr * $td . "</td>";
+
+        }
+
+        $multiTable .= "</tr>";
+    }
+
+    $multiTable .= "</table>";
+
+    return $multiTable;
 }
