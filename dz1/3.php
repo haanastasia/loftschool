60f5203e4fc0d1b41435130d9a24ef6c0840d47a
Anastasia Kharitonova <haanastasia@yandex.com>
2021-01-17 00:14:35 +0400

    Подправила условие для третьего задания

--- dz1/3.php
+++ dz1/3.php
@@ -7,7 +7,7 @@ if ($age >= 18 and $age <= 65) {
     echo 'Вам еще работать и работать';
 } elseif ($age > 65) {
     echo 'Вам пора на пенсию';
-} elseif ($age < 18) {
+} elseif ($age >= 0 and $age < 18) {
     echo 'Вам ещё рано работать';
 } else {
     echo 'Неизвестный возраст';
